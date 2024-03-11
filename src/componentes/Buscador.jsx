380d10720d89aeb3b4bc7200bf7a71bbd0d21da6
ERAZOLAR <132466470+ERAZOLAR@users.noreply.github.com>
2024-03-11 14:16:19 -0500

    creacion de Buscador por RAMA Buscador

--- src/componentes/Buscador.jsx
+++ src/componentes/Buscador.jsx
@@ -1,13 +1,24 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-const Buscador = () => {
-    
-        return (
-            <div>
-                
-            </div>
-        );
-    
-}
+const Buscador = ({ onFiltrar }) => {
+  const [busqueda, setBusqueda] = useState('');
+
+  const handleBusqueda = (e) => {
+    const valor = e.target.value;
+    setBusqueda(valor);
+    onFiltrar(valor);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder=" .. Buscar por NOMBRE ¡!"
+        value={busqueda}
+        onChange={handleBusqueda}
+      />
+    </div>
+  );
+};
 
 export default Buscador;

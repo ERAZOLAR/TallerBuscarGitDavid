a93d3816a7ef8945a7c596509074cc80d4bae06e
ERAZOLAR <132466470+ERAZOLAR@users.noreply.github.com>
2024-03-11 14:12:19 -0500

    creacion de tabla rama master

--- src/componentes/Tabla.jsx
+++ src/componentes/Tabla.jsx
@@ -1,13 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-const Tabla = () =>{
-    
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+const Tabla = ({ materias }) => {
+    return (
+      <div>
+          <h6 className='TituloTabla'>" TABLA  DE INFORMACION CON MATERIAS ENCONTRADAS " </h6>
+        <table>
+          <thead>
+            <tr className='TituloColumnasTabla'>
+              
+              <th>ID</th>
+              <th>Nombre</th>
+              <th>Descripción</th>
+            </tr>
+          </thead>
+          <tbody>
+            {materias.map((materia) => (
+              <tr key={materia.id}>
+                <td>{materia.id}</td>
+                <td>{materia.nombre}</td>
+                <td>{materia.descripcion}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  };
 
 
 export default Tabla;
